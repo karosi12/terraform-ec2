2f725b530b8f72af9a013730e496cf8fe62427a0
karosi12 <adekayor@gmail.com>
2023-11-15 00:37:33 +0200

    feat: added firewall

--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@ variable vpc_cidr_block {}
 variable subnet_cidr_block {}
 variable avail_zone {}
 variable env-prefix {}
+variable my-ip {}
 
 
 resource "aws_vpc" "myapp-vpc" {
@@ -46,4 +47,34 @@ resource "aws_route_table" "myapp-route-aws_route_table" {
 resource "aws_route_table_association" "a-rtb-subnet" {
   subnet_id = aws_subnet.myapp-vpc-subnet-1.id
   route_table_id = aws_route_table.myapp-route-aws_route_table.id
+}
+
+resource "aws_security_group" "myapp-sg" {
+  name = "myapp-sg"
+  vpc_id = aws_vpc.myapp-vpc.id
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = [ var.my-ip ]
+  }
+
+    ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = [ "0.0.0.0/0" ]
+    prefix_list_ids = []
+  }
+
+  tags =  {
+    Name: "${var.env-prefix}-sg"
+  }
 }
